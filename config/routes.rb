59e03ce617b2eb853fba29650c63343bb4864325
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-12 22:25:00 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  resources :questions do
+      resources :answers do
+        member do
+          get 'toggle', as: :toggle
+        end
+      end
+    end
   get 'pages/welcome'
+
+  root 'pages#welcome'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
